660bd33e6d149d4a1be253187fd9ceccd01817f7
Alex Boyd <alexanderleeboyd@gmail.com>
2022-09-07 21:58:06 -0400

    Feat: landing page does not display login or create user if user is logged in

--- spec/features/sessions/new_spec.rb
+++ spec/features/sessions/new_spec.rb
@@ -81,4 +81,20 @@ RSpec.describe "log in page", type: :feature do
     expect(page).to have_content("Error: please fill in all fields")
   end
 
+  it 'does not display a link to log in or create an account if user is logged in' do
+    user1 = User.create!(first_name: "David", last_name: "Lynch", email: "david-fake@test.com", password: "iluvmovies123", password_confirmation: "iluvmovies123")
+
+    visit login_path
+
+    fill_in "Email", with: "david-fake@test.com"
+    fill_in "Password", with: "iluvmovies123"
+
+    click_on "Log In"
+
+    expect(current_path).to eq(root_path)
+
+    expect(page).to_not have_content("Create a New User")
+    expect(page).to_not have_content("Log In")
+  end
+
 end
